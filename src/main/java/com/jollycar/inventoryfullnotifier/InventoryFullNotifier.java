c8b0c23a0c464b51c2ff69911eded44536720a46
jollycar <939345+jollycar@users.noreply.github.com>
2023-06-29 21:47:31 +0200

    Organise packages, set versions, set mod icon

--- src/main/java/com/jollycar/inventoryfullnotifier/InventoryFullNotifier.java
+++ src/main/java/com/jollycar/inventoryfullnotifier/InventoryFullNotifier.java
@@ -1,22 +1,15 @@
 package com.jollycar.inventoryfullnotifier;
 
-import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class InventoryFullNotifier implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("inventoryfullnotifier");
+public class InventoryFullNotifier implements ClientModInitializer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("inventoryfullnotifier");
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
 }
